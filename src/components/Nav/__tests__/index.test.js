eb9f957b426b828a0a461b3cb82d74c6b766e9d1
Yussef Rafat <yussefrafat5908@gmail.com>
2021-10-02 15:54:18 -0400

    files

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -12,18 +13,18 @@ describe('Nav component', () => {
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-  const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav />);
 
-  expect(getByLabelText('camera')).toHaveTextContent('📸');
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
